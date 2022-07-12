16368cef0cf9efc6b653ecea5693845af001cf2d
siddhantraotole <93128003+siddhantraotole@users.noreply.github.com>
2022-07-12 18:25:22 +0530

    Create printHello.groovy

---
+++ vars/printHello.groovy
@@ -0,0 +1,6 @@
+def call(Map parameters = [:]) {
+def printHelloScript = libraryResource '/print-hello.sh'
+def name = parameters.name
+  withEnv(["NAME=${name}])
+           sh(printHelloScript)
+ }
